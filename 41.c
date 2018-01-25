cd1290362b5945a5873ff671965065bb667f40de
922515 <35562149+922515@users.noreply.github.com>
2018-01-25 11:14:10 +0000

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,5 @@
+scanf("%d:%d:%d%s", &hh, &mm, &ss, tt);
+    printf("%d\n", hh);
+    if(strcmp(tt, "PM") == 0 && hh != 12) hh += 12;
+    if(strcmp(tt, "AM") == 0 && hh == 12) hh = 0;
+    printf("%02d:%02d:%02d", hh, m
