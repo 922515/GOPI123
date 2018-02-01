a6ab57d3fb0a4316582f0bf9885fa7b69e89bbe4
922515 <35562149+922515@users.noreply.github.com>
2018-02-01 15:09:47 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,Product;
+clrscr();
+printf("Enter Two Numbers\n");
+scanf("\n%d\n%d",&a,&b);
+Product=a*b;
+printf("\nProduct is =%d",Product);
+getch();
+retur
