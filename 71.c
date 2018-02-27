8e8a69c7a9689b1b36653f122bc65cdf4f25dd6a
r gopinath <35562149+922515@users.noreply.github.com>
2018-02-27 16:19:46 +0530

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s1[10],s2[10];
+scanf("%s%s",&s1,&s2);
+s1=s2;
+{
+printf("the two string is palindrome%s",c);
+}
+else
+{
+printf("the chr is not string%s",c);
+}
+return 0;
+}
