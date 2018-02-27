09c7a80ce3619d7d459cc4b3a93a1053c5f90234
r gopinath <35562149+922515@users.noreply.github.com>
2018-02-27 16:03:24 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,c;
+c=a/7;
+scanf("%d",&a);
+if(n==1)
+{
+printf("the value is divided by 7%d",c);
+}
+else
+{
+printf("the value is not divided by 7%d",c);
+}
+return 0;
+}
