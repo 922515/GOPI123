699894d2dcdb2f27832d7d457ca954cad747aea2
r gopinath <35562149+922515@users.noreply.github.com>
2018-03-01 16:16:56 +0530

    Rename 62.c to 6 squre.c

---
+++ 6 squre.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+int a,b,n1,n2;
+n1=a*a;
+n2=b*b;
+if(n1<n2&&n2<n1)
+{
+	printf("the greatest square is %d",n1);
+	
+}
+else
+{
+printf("the gretest squre is %d",n2);
+}
+	return 0;
+}
