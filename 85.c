06b554c41d567bc6230222072eea72631203aaca
r gopinath <35562149+922515@users.noreply.github.com>
2018-03-08 16:26:20 +0530

    Create 85.c

---
+++ 85.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c,d,e,f,g,h,i;
+scanf("%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f);
+g=a-b;
+h=c-d;
+i=e-f;
+printf("\n %d",g);
+printf("\n %d",h);
+printf("\n %d",i);
+	return 0;
+}
