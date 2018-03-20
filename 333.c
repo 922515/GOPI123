7e1ea3edd2563f5bb3523ccc7369aa51d1fbf1b1
r gopinath <35562149+922515@users.noreply.github.com>
2018-03-20 16:31:55 +0530

    Create 333.c

---
+++ 333.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#define MAX 100
+
+int main()
+{
+	char str[MAX]={0};	
+	int i;
+	
+	//input string
+	printf("Enter a string: ");
+	scanf("%[^\n]s",str); //read string with spaces
+	
+	//capitalize first character of words
+	for(i=0; str[i]!='\0'; i++)
+	{
+		//check first character is lowercase alphabet
+		if(i==0)
+		{
+			if((str[i]>='a' && str[i]<='z'))
+				str[i]=str[i]-32; //subtract 32 to make it capital
+			continue; //continue to the loop
+		}
+		if(str[i]==' ')ound, check next character
+			++i;
+			//check next character is lowercase alphabet
+			if(str[i]>='a' && str[i]<='z')
+			{
+				str[i]=str[i]-32; //subtract 32 to make it capital
+				continue; //continue to the loop
+			}
+		}
